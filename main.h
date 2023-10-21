c85bca3712e0766b080c884db5497924a37d8055
cuthyiscool <cuthbertn15@gmail.com>
2023-10-20 17:56:03 -0700

    print_%.c

--- main.h
+++ main.h
@@ -1,30 +1,47 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define BUFF_SIZE 1024
 
+
+/**
+ * struct format - converter for printf
+ * @ph: type char pointer of the specifier
+ * @function: function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *ph;
+	int (*function)();
+} convert;
+
+int *_strcpy(char *dest, char *src);
+int print_pointer(va_list val);
+int print_unsigned(va_list args);
+int print_hex_extra(unsigned long int num);
+int _strlenc(const char *s);
+int print_HEX_extra(unsigned int num);
+int print_exc_string(va_list val);
+int print_HEX(va_list val);
+int print_hex(va_list val);
+int print_oct(va_list val);
+int print_bin(va_list val);
+int print_revs(va_list args);
+int print_rot13(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
+int _strlen(char *s);
+int rev_string(char *s);
+int print_37(void);
+int print_c(va_list val);
+int print_s(va_list val);
 int _putchar(char c);
-
-int printf_int(int arg);
-
-int printf_char(char arg);
-
-int printf_string(char *arg);
-
-int printf_octal(unsigned int arg);
-
-int printf_binary(unsigned int arg);
-
 int _printf(const char *format, ...);
 
-int printf_uppercase(unsigned int arg);
-
-int printf_hexadecimal(unsigned int arg);
-
-int printf_unsigned_int(unsigned int arg);
-
-int functions_handler(const char *format, va_list args);
-
-#endif /*MAIN.H*/
+#endif
